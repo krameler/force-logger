bc0cf7ac663b50d9e33fa288c847393f3f5a1f1f
krameler <philipp@huschauer.de>
2020-01-03 02:44:54 +0100

    Fix search for available scale_id
    Broke when already connected to 6 scales
    or when scale_id didn't get reset

--- server.py
+++ server.py
@@ -41,7 +41,6 @@ def clientThread(client_id, conn, client_ip, port):
 
 
 def startServer():
-    scale_id = 0
     soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # this is for easy starting/killing the app
     soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -66,16 +65,19 @@ def startServer():
     while True:
         conn, addr = soc.accept()
         ip, port = str(addr[0]), str(addr[1])
-        while True:  # checking for available scale ids
-            if variables.list_scale_id[scale_id] == False:
-                break
-            scale_id += 1
+
+        #checking for available scale ids
+        try:
+            scale_id = variables.list_scale_id.index(False)
+        except ValueError:
+            print('Connection refused: No available scale_id found!')
+            conn.close()
+            continue
         print('Accepting connection from ' + ip + ':' + port + ' Id is:' + str(scale_id))
         try:
 
             Thread(target=clientThread, args=(scale_id, conn, ip, port), daemon=True).start()
             variables.list_scale_id[scale_id] = True
-            scale_id = 0
         except:
             print("Terrible error!")
             import traceback
