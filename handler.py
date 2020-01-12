9edd42c2e061e695e5a5a4c1ba89ef9bfed56b62
krameler <philipp@huschauer.de>
2020-01-12 17:22:31 +0100

    Refactor handlerThread

--- handler.py
+++ handler.py
@@ -7,49 +7,44 @@ import queue
 # import main_window
 
 def handlerThread():
-    client_id = 0
-    client_msg = None
-
     while True:
-        for client_id in range(5):
+        for client_id in range(6):
             # print("looping")
             if variables.list_scale_id[client_id] == True:
 
                 try:
-                    client_msg = str(variables.queues_recv[client_id].get(False))
-                    variables.queues_recv[client_id].task_done()
-
-                    if client_msg != None:
-
-                        if str(client_msg).count("mes"):
-                            parts = client_msg.split(":")
-                            if variables.en_log == True:
-                                variables.list_mes_file[client_id].write(parts[2] + ":" + parts[3] + "\n")
-                            # print("test")
-                            variables.list_scale_mom[client_id] = parts[3]
-                            print(str(parts[3]))
-                            client_msg = ""
-
-                        elif str(client_msg).count("ack"):
-                            # print("Acknowlege")
-                            variables.queues_ack[client_id].put(True)
-                            client_msg = ""
-                        '''   
-                        elif client_msg.count("ip"):
-                            list_client_ip[client_id] = client_msg
-                            
-                        elif str(client_msg).count("blk"):
-                            f = open("log/" + filename + "/" + filename + "_ID" + str(client_id) + ".blk", "a+")
-                            f.write(parts[2])
-                            f.close()
-                        else:
-                            #print("Wrong command")
-                            #print(client_msg)
-                            client_msg = None'''
+                    client_msg = variables.queues_recv[client_id].get(False)
                 except queue.Empty:
-                    pass
+                    continue
 
-        client_id = 0
+                if client_msg != "":
+
+                    if client_msg.count("mes"):
+                        parts = client_msg.split(":")
+                        if variables.en_log == True:
+                            variables.list_mes_file[client_id].write(parts[2] + ":" + parts[3] + "\n")
+                        # print("test")
+                        variables.list_scale_mom[client_id] = parts[3]
+                        # print(str(parts[3]))
+
+                    elif str(client_msg).count("ack"):
+                        # print("Acknowlege")
+                        variables.queues_ack[client_id].put(True)
+
+                    '''   
+                    elif client_msg.count("ip"):
+                        list_client_ip[client_id] = client_msg
+                        
+                    elif str(client_msg).count("blk"):
+                        f = open("log/" + filename + "/" + filename + "_ID" + str(client_id) + ".blk", "a+")
+                        f.write(parts[2])
+                        f.close()
+                    else:
+                        #print("Wrong command")
+                        #print(client_msg)
+                        client_msg = None'''
+
+                variables.queues_recv[client_id].task_done()
         # time.sleep(0.1)
 
 def startCalibration(client_id):
